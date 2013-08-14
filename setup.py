214d2527dc7a7e2470850e7d23c43af2f2921f83
Frank Lam <frank@duckpun.ch>
2013-08-13 23:25:51 -0700

    upped version and pushed to pypi

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='osx-powerline-battery-segment',
-    version='0.1',
+    version='0.1.1',
     author='Frank Lam',
     author_email='frank@duckpun.ch',
     packages=['osx_powerline_battery'],
     description='Battery segment for powerline in OSX',
+    url='https://github.com/fathwad/osx-powerline-battery',
 )
