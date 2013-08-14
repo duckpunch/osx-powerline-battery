faae5f30767299b8ed427631ada2eac7638cd297
Frank Lam <frank@duckpun.ch>
2013-08-13 23:48:29 -0700

    No need to split the battery summary

--- osx_powerline_battery/segments.py
+++ osx_powerline_battery/segments.py
@@ -4,11 +4,11 @@ import subprocess, re
 def osx_battery(pl, steps=4, gamify=True):
     full_heart = '♥ '
     empty_heart = '♡ '
-    power_type, charge, _ = subprocess.check_output(['pmset', '-g', 'batt']).decode('utf8').split('\n')
-    if 'AC Power' in power_type:
+    battery_summary = subprocess.check_output(['pmset', '-g', 'batt']).decode('utf8')
+    if 'AC Power' in battery_summary:
         return None
     else:
-        batt_percent = re.search(r'\d+%', charge).group(0)
+        batt_percent = re.search(r'\d+%', battery_summary).group(0)
         batt_level = int(batt_percent.replace('%', ''))
         step_size = 100.0/steps
         num_of_full_hearts = min(int(batt_level/step_size) + 1, steps)
